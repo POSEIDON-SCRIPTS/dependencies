29e499d609b15ecbbfce2e422fca6fc029ab7c0e
safser2 <safsernicolas@gmail.com>
2025-08-11 09:11:16 +0300

    UPDATE vorp_menu

--- vorp_menu/fxmanifest.lua
+++ vorp_menu/fxmanifest.lua
@@ -1,7 +1,11 @@
-fx_version 'adamant'
+fx_version 'cerulean'
 game 'rdr3'
-author 'vorp @outsider' --  SLIZZARN original author
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author 'VORP edit by @outsider' --  SLIZZARN original author
+description 'A tool to build RedM menus for your scripts'
+repository 'https://github.com/VORPCORE/vorp_menu'
+lua54 'yes'
 
 client_scripts {
 	'client/main.lua'
@@ -19,3 +23,8 @@ files {
 	'html/js/app.js',
 	'html/fonts/*ttf',
 }
+
+version '1.2'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/VORPCORE/vorp_menu'
